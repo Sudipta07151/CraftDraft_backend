390ef747be4403fa1a4db69d93367c15100236f2
Sudipta Adak <adak07151@gmail.com>
2021-07-07 02:26:16 +0530

    add

---
+++ my-strapi-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3054b5e36e3890c884edc993583c86d9'),
+    },
+  },
+});
---
+++ my-strapi-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a2ce5e1d-9815-4de9-afcb-dcbf138c5ef7'
+};
